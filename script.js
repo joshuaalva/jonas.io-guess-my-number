c523f53b955e0ec73cc8e291ad3ffa58b2a8bf6e
Joshua Alvarado <joshalva3@gmail.com>
2023-06-30 02:30:09 -0500

    handling click events

--- script.js
+++ script.js
@@ -15,13 +15,28 @@
 // Dom Methods and Properties - Web APIs
 
 // Selecting and Manipulationg Elements
-console.log(document.querySelector('.message').textContent); // start guessing...
+// console.log(document.querySelector('.message').textContent); // start guessing...
 // changes the message text content from start guessing... > correct number
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent); // correct number
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent); // correct number
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-console.log((document.querySelector('.guess').value = 23));
+// console.log((document.querySelector('.guess').value = 23));
 
-// Handling Click Events
+// 73 Handling Click Events // 74 Implementing the game logic
+
+// event listener
+// event is something that happens on the page
+// can wait for an event to happen and then react to it
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
+// method so needs ()
+// addEventlistener is the most used
