a47a501e2211325b9d88bab300cdb83f79eb2ef9
Joshua Alvarado <joshalva3@gmail.com>
2023-07-03 23:53:56 -0500

    implementing high scores

--- script.js
+++ script.js
@@ -31,6 +31,7 @@
 // can wait for an event to happen and then react to it
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -47,6 +48,11 @@ document.querySelector('.check').addEventListener('click', function () {
     // have to specify a string when using these selectors
     document.querySelector('body').style.backgroundColor = `#60b347`;
     document.querySelector('.number').style.width = `30rem`;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess > secretNumber) {
     // when guess it too high
     if (score > 1) {
@@ -70,6 +76,12 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+// method so needs ()
+// addEventlistener is the most used
+// Manipulating CSS styles
+// coding challenge #1
+// again button functionality
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -83,6 +95,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = `15rem`;
 });
 
-// method so needs ()
-// addEventlistener is the most used
-// Manipulating CSS styles
+// Implementing High Scores
